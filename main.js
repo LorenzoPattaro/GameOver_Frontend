f641def2c8d0b664f44253786298a535e0c88a9b
Lorenzo Pattaro <lorenzopattaro@live.it>
2024-12-18 21:48:40 +0100

    aggiunta header e sezioni categorie, i nostri numeri

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let navbar = document.querySelector("#navbar");
 let logo = document.querySelector("#logo")
 
 
+// evento scroll per modificare la navbar
 window.addEventListener( "scroll", () => {
 
         logo.style.transform = `rotateZ(${window.scrollY}deg)`;
@@ -17,5 +18,37 @@ window.addEventListener( "scroll", () => {
         }
 
 })
+
+
+let numGiochi = document.querySelector("#numGiochi")
+let numClienti = document.querySelector("#numClienti")
+let numGiochiNuovi = document.querySelector("#numGiochiNuovi")
+
+
+// funzione per creare un intervallo di tempo in cui stampa i numeri della sezione I NOSTRI NUMERI
+function intervalloNostriNumeri (numero, elemento, tempo) {
+
+    let counter = 0;
+
+    let intervallo = setInterval( () => {
+
+        if(counter < numero){
+
+            counter ++;
+
+            elemento.innerHTML =`${counter}+`
+
+        } else {
+
+            clearInterval(intervallo)
+        }
+
+    },tempo)
+}
+
+intervalloNostriNumeri(100, numGiochi, 70);
+intervalloNostriNumeri(250, numClienti, 28);
+intervalloNostriNumeri(30, numGiochiNuovi, 230);
+
 
 
