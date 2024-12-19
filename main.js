6b64e9436fcdeaca81f0395ec6df7b867c0cdc03
Lorenzo Pattaro <lorenzopattaro@live.it>
2024-12-19 19:39:44 +0100

    aggiunta sezione cards

--- main.js
+++ main.js
@@ -1,5 +1,64 @@
+
+let cards  = [
+
+            {"titolo": "The Last Of US", "descrizione": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum eos sed maiores, placeat reprehenderit iste explicabo corporis omnis", "categoria": "Sopravvivenza", "dataPubblicazione": "12/10/2002", "copertina": "tlou.avif"},
+
+            {"titolo": "Read Dead Redemption ll", "descrizione": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum eos sed maiores, placeat reprehenderit iste explicabo corporis omnis", "categoria": "Sopravvivenza", "dataPubblicazione": "12/10/2002", "copertina": "rdr2.avif"},
+
+            {"titolo": "Rainbow Six Siege", "descrizione": "lLorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum eos sed maiores, placeat reprehenderit iste explicabo corporis omnis", "categoria": "Sopravvivenza", "dataPubblicazione": "12/10/2002", "copertina": "r6.avif"}
+
+];
+
+let cardsWrapper = document.querySelector("#cardsWrapper");
+
+
+
+// visualizzazione cards a schermo
+
+
+
+cards.forEach ( card => {
+
+        let div = document.createElement("div");
+        div.classList.add("col-3", "card", "border", "border-white", "border-5", "bg-color-a", "p-0", "text-center");
+        // div.style.width = "18rem";
+
+        div.innerHTML = `
+                    <figure>
+                    <img src="./media/${card.copertina}" class="card-img-top" alt="...">
+                    </figure>
+                    
+                    <div class="card-body">
+                    <h4 class="card-title title color-s text-color-p-shadow fw-bold pb-3 mb-3 border-bottom border-2 ">${card.titolo}</h5>
+                    <p class="card-text color-s">
+                        ${card.descrizione}
+                    </p>
+                    <a href="#" class="btn btn-outline-light">Più Informazioni</a>
+                    </div>
+        `
+        
+        cardsWrapper.append(div)
+    } )
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let navbar = document.querySelector("#navbar");
-let logo = document.querySelector("#logo")
+let logo = document.querySelector("#logo");
 
 
 // evento scroll per modificare la navbar
@@ -18,6 +77,9 @@ window.addEventListener( "scroll", () => {
         }
 
 })
+
+
+
 
 
 let numGiochi = document.querySelector("#numGiochi")
@@ -46,9 +108,30 @@ function intervalloNostriNumeri (numero, elemento, tempo) {
     },tempo)
 }
 
-intervalloNostriNumeri(100, numGiochi, 70);
-intervalloNostriNumeri(250, numClienti, 28);
-intervalloNostriNumeri(30, numGiochiNuovi, 230);
 
+let osservatoreControllo = true;
 
+// IntersectionObserver per far partire il counter da un punto specifico del sito 
+let osservatore = new IntersectionObserver( entries => {
+
+    if(osservatoreControllo == true) {
+
+        entries.forEach (entry =>{
+
+            if(entry.isIntersecting){
+
+                
+                intervalloNostriNumeri(100, numGiochi, 70);
+                intervalloNostriNumeri(250, numClienti, 28);
+                intervalloNostriNumeri(30, numGiochiNuovi, 230);
+
+                osservatoreControllo = false;
+            }
+
+        } )
+
+    }
+})
+
+osservatore.observe(numGiochi)
 
